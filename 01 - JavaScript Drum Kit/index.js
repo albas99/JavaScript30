d2a8391b6caa3fc7edecb463d6ccac6e7112a513
Faisal Albasu <albasfaisal@yandex.ru>
2021-06-02 10:48:31 +0400

    added javascript functionality

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,21 @@
+playSound = (e) => {
+const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!audio) return; //stop the function from running altogether if there's no audio related to the keycode
+    audio.currentTime = 0; //rewind audio to the start any time key is hit again
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; //skip if transition property is not a transform
+
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+window.addEventListener('keydown', playSound);
